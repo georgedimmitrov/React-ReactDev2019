80da2fc08e75d23169c61ee54719a3e16f79331b
Georgi Dimitrov <dgeorgi@vmware.com>
2019-11-11 23:19:10 +0200

    Updating stripe integration and updating our favicon and title

--- crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
@@ -42,6 +43,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
